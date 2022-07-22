5920a06a9f4b05cd3c00a8e3346a788b50ee3f52
emon273273 <emon.cse273@gmail.com>
2022-07-22 12:48:47 +0600

    jul 22 2022

---
+++ star_pattern.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+
+
+	cout<<"emon"<<endl;
+
+
+
+	int i,j;
+
+	for(i=0;i<5;i++){
+
+     for(j=0;j<5;j++){
+if(i>=j){
+cout<<"*";
+
+}else{
+
+	cout<<" ";
+}
+     	
+     }
+
+     cout<<endl; 		
+	}
+}  
